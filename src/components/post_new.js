8f548d787b0f3e1ec08bb584a7006b6e4cf628a2
Leonardo Bruno Lima <leonardo.bruno@outlook.com>
2018-02-01 14:55:50 -0300

    add redux-form to create post page.

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,13 +1,80 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostNew extends Component {
-    render() {
+
+    renderField(field) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger': ''}`;
+
         return (
-            <div>
-                PostNew!
+            <div className={className}>
+                <label>{field.label}</label>
+                <input className="form-control"
+                    type="text"
+                    // onChange={field.input.onChange}
+                    // onFocus={field.input.onFocus}
+                    {...field.input}
+                />
+
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
+        )    
+    }
+
+    onSubmit(values) {
+        console.log(values);
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="" className="btn btn-danger">Cancel</Link>
+            </form>
         )
     }
 }
 
-export default PostNew;
+function validate(values) {
+    // console.log(values);    
+
+    const errors = {};
+
+    if (!values.title)
+        errors.title = "Enter a title!";
+    
+    if (!values.categories)
+        errors.categories = "Enter some categories!";
+
+    if (!values.content)
+        errors.content = "Enter a content!";
+
+    // if errors is empty, the form is fine to be submited, otherwise redux-form assumes form is invalid
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostNew);
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import { Link } from 'react-router-dom';
 
 class PostsIndex extends Component {
   componentDidMount() {
@@ -24,6 +25,11 @@ class PostsIndex extends Component {
 
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
